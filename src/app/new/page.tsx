f0743b53f1f08c49a2588e8436e4a09b100bcb59
Sarmad rafique <sarmadrafique040@gmail.com>
2024-01-26 10:43:51 +0500

    Added input states, separated the server action

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,30 +1,29 @@
-import prisma from "@/lib/db";
+"use client";
+
 import Navbar from "@/components/Navbar new";
-import { redirect } from "next/navigation";
+import { useState } from "react";
+import { addBlog } from "@/actions/actions";
 const NewBlog = () => {
-  const addBlog = async (event: FormData) => {
-    "use server";
-    const title = event.get("title")?.valueOf();
-    const content = event.get("content")?.valueOf();
-    if (typeof title === "string" && typeof content === "string") {
-      await prisma.blog.create({
-        data: { title, content },
-      });
-      redirect("/");
-    } else {
-      throw new Error("Cannot Create A New Blog. Please Provide Valid Data");
-    }
-  };
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <div>
       <Navbar />
       <div className="text-center m-auto my-32">
-        <form action={addBlog}>
+        <form
+          action={addBlog}
+          onSubmit={() => {
+            setTitle("");
+            setContent("");
+          }}
+        >
           <input
             type="text"
             className="border border-gray-700 mx-auto lg:w-1/3 w-full p-2 rounded-md block my-10"
             placeholder="Title"
             name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             id="title"
             required
           />
@@ -39,7 +38,10 @@ const NewBlog = () => {
             rows={60}
             cols={50}
             maxLength={160}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             required
+            spellCheck
           ></textarea>
           <button
             type="submit"
